d1cae5572639482fa1c922914ba87b371a1de0c8
kimin227 <kiminjeong227@gmail.com>
2022-12-01 16:03:14 +0900

    implement cont training

--- train.py
+++ train.py
@@ -8,13 +8,11 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-start_iter = 1
-num_epochs = 10
-batch_size = 512
+
+batch_size = 256
 learning_rate = 1e-3
 data_dir = '../dataset'
 device  = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 
 train_dataset, valid_dataset, class_to_idx = load_dataset(dataset='mnist', data_dir=data_dir)
@@ -29,7 +27,28 @@ train_accuracy_meter = AverageMeter()
 test_loss_meter = AverageMeter()
 test_accuracy_meter = AverageMeter()
 
-for epoch in range(start_iter, start_iter + num_epochs):
+save_freq = 1
+eval_freq = 1
+num_epochs = 50
+
+
+save_dir = './save'
+cont_training = False
+
+if cont_training:
+    state_dict = load(save_dir)
+    init_epoch = state_dict['epoch']
+    global_step = state_dict['global_step']
+    model.load_state_dict(state_dict['model'])
+    optimizer.load_state_dict(state_dict['optimizer'])
+
+else:
+    init_epoch = 1
+    global_step = 0
+
+
+
+for epoch in range(init_epoch, num_epochs + 1):
     
     # train
     model.train()
@@ -37,8 +56,10 @@ for epoch in range(start_iter, start_iter + num_epochs):
     train_accuracy_meter.reset()
     pbar = tqdm(enumerate(train_loader), leave=False)
     
+    log_str = f'epoch {str(epoch).zfill(3)}'
+    
     for i, (x, y) in pbar:
-        pbar.set_description(f'epoch {epoch} train [{i + 1}/{len(train_loader)}]')
+        pbar.set_description(log_str + f' | train [{i + 1}/{len(train_loader)}]')
         x = x.to(device)
         y = y.to(device)
         optimizer.zero_grad()
@@ -47,30 +68,42 @@ for epoch in range(start_iter, start_iter + num_epochs):
         loss = - log_p.mean()
         loss.backward()
         optimizer.step()
+        global_step += 1
         accuracy = match_accuracy(log_cls, y).float().mean()
         train_loss_meter.update(loss.item(), n=x.size()[0])
         train_accuracy_meter.update(accuracy.item(), n=x.size()[0])
+
+    log_str = log_str + f' | train loss: {train_loss_meter.avg: .5f} | train accuracy: {train_accuracy_meter.avg: .5f}'
+    
+    if epoch % save_freq == 0 or epoch == num_epochs:
         
+        state_dict = {'epoch': epoch + 1, 
+                      'global_step': global_step,
+                      'model': model.state_dict(),
+                      'optimizer': optimizer.state_dict()}
+
+        save(state_dict, save_dir='./save')
     
     # test
-    model.eval()
-    test_loss_meter.reset()
-    test_accuracy_meter.reset()
-    pbar = tqdm(enumerate(test_loader), leave=False)
+    if epoch % eval_freq == 0 or epoch == num_epochs:
+        model.eval()
+        test_loss_meter.reset()
+        test_accuracy_meter.reset()
+        pbar = tqdm(enumerate(test_loader), leave=False)
+
+        with torch.no_grad():
+            for i, (x, y) in pbar:
+                pbar.set_description(log_str + f' | test [{i + 1}/{len(test_loader)}]')
+                x = x.to(device)
+                y = y.to(device)
+                log_cls = model(x)
+                log_p = log_prob(log_cls, y)
+                loss = - log_p.mean()
+                accuracy = match_accuracy(log_cls, y).float().mean()
+                test_loss_meter.update(loss.item(), n=x.size()[0])
+                test_accuracy_meter.update(accuracy.item(), n=x.size()[0])
+                
+        log_str = log_str + f' | valid loss: {test_loss_meter.avg: .5f} | valid accuracy: {test_accuracy_meter.avg: .5f}'
     
-    with torch.no_grad():
-        for i, (x, y) in pbar:
-            pbar.set_description(f'epoch {epoch} test [{i + 1}/{len(test_loader)}]')
-            x = x.to(device)
-            y = y.to(device)
-            log_cls = model(x)
-            log_p = log_prob(log_cls, y)
-            loss = - log_p.mean()
-            accuracy = match_accuracy(log_cls, y).float().mean()
-            test_loss_meter.update(loss.item(), n=x.size()[0])
-            test_accuracy_meter.update(accuracy.item(), n=x.size()[0])
-            
     
-    print(f'epoch {epoch}'
-          + f' | train loss: {train_loss_meter.avg: .5f} | train accuracy: {train_accuracy_meter.avg: .5f}'
-          + f' | valid loss: {test_loss_meter.avg: .5f} | valid accuracy: {test_accuracy_meter.avg: .5f}')
+    print(log_str)
--- utils.py
+++ utils.py
@@ -17,35 +17,49 @@ class AverageMeter(object):
         self.avg = self.sum / self.cnt
         
 
-def save(model, ema_model=None, optimizer=None, ckpt_dir='./checkpoint', file_name='model.pth'):
+# def save(model, ema_model=None, optimizer=None, ckpt_dir='./checkpoint', file_name='model.pth'):
     
-    assert os.path.splitext(file_name)[-1] == '.pth', 'invalid file_name'
+#     assert os.path.splitext(file_name)[-1] == '.pth', 'invalid file_name'
     
-    if not os.path.exists(ckpt_dir):
-        print(f'make directory: {ckpt_dir}')
-        os.makedirs(ckpt_dir)
+#     if not os.path.exists(ckpt_dir):
+#         print(f'make directory: {ckpt_dir}')
+#         os.makedirs(ckpt_dir)
     
-    state_dict = {'model': model.state_dict()}
+#     state_dict = {'model': model.state_dict()}
 
-    if ema_model is not None:
-        state_dict.update({'ema_model': ema_model.state_dict()})
+#     if ema_model is not None:
+#         state_dict.update({'ema_model': ema_model.state_dict()})
 
-    if optimizer is not None:
-        state_dict.update({'optimizer': optimizer.state_dict()})
+#     if optimizer is not None:
+#         state_dict.update({'optimizer': optimizer.state_dict()})
         
-    torch.save(state_dict, f=f'{ckpt_dir}/{file_name}')
+#     torch.save(state_dict, f=f'{ckpt_dir}/{file_name}')
     
 
 
-def load(model, ema_model=None, optimizer=None, ckpt_dir='./checkpoint', file_name='model.pth'):
+# def load(model, ema_model=None, optimizer=None, ckpt_dir='./checkpoint', file_name='model.pth'):
     
-    assert os.path.splitext(file_name)[-1] == '.pth', 'invalid file_name'
+#     assert os.path.splitext(file_name)[-1] == '.pth', 'invalid file_name'
     
-    state_dict = torch.load(f'{ckpt_dir}/{file_name}')
-    model.load_state_dict(state_dict['model'])
-    if ema_model is not None:
-        ema_model.load_state_dict(state_dict['ema_model'])
-    if optimizer is not None:
-        optimizer.load_state_dict(state_dict['optimizer'])
+#     state_dict = torch.load(f'{ckpt_dir}/{file_name}')
+#     model.load_state_dict(state_dict['model'])
+#     if ema_model is not None:
+#         ema_model.load_state_dict(state_dict['ema_model'])
+#     if optimizer is not None:
+#         optimizer.load_state_dict(state_dict['optimizer'])
 
-    return model, ema_model, optimizer
+#     return model, ema_model, optimizer
+
+
+
+def save(state_dict, save_dir='./save'):
+    if not os.path.exists(save_dir):
+        print(f'make directory: {save_dir}')
+        os.makedirs(save_dir)
+    
+    torch.save(state_dict, f=f'{save_dir}/checkpoint.pt')
+
+
+def load(save_dir='./save'):
+    state_dict = torch.load(f'{save_dir}/checkpoint.pt')
+    return state_dict
